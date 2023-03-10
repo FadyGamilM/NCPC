2e04fcd1dd1a0d80999dbdf3db53e9a46144d484
fady <gamilfady605@gmail.com>
2023-03-10 17:15:02 +0200

    slider is done

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,11 @@
 import React from "react";
-// import {} from "react"
+import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
 
 const Header = () => {
 	return (
-		<div className="border-b-4 border-slate-600 mb-8 pb-2 grid grid-cols-2 text-center">
-			{/* Logo */}
+		// <div className="border-b-4 border-slate-600 mb-8 pb-2 grid grid-cols-2 text-center">
+		<div className="border-b-4 border-slate-600 mb-8 pb-2 flex justify-between text-center items-center">
+			{/*//! Logo */}
 			<div>
 				<img
 					className="w-28 h-28 shadow-xl rounded-lg hover:cursor-pointer"
@@ -12,7 +13,7 @@ const Header = () => {
 					alt="logo"
 				/>
 			</div>
-			{/* Menu */}
+			{/*//! Menu */}
 			<div className=" flex items-center text-center justify-center">
 				<ul className="flex gap-16">
 					<li className="shadow-xl p-2 rounded-2xl bg-slate-100 font-standard text-slate-800 text-2xl font-bold cursor-pointer hover:text-slate-900 hover:scale-110 transition ease-in-out hover:underline hover:underline-offset-8">
@@ -26,6 +27,26 @@ const Header = () => {
 					</li>
 				</ul>
 			</div>
+
+			{/* //! Mobile Menu icon */}
+			{/* <div>
+				<AiOutlineMenu className="font-bold text-3xl hover:scale-110 transition ease-in-out hover:cursor-pointer" />
+			</div> */}
+
+			{/* //! Mobile Menu Navigation */}
+			{/* <div className="fixed bg-gray-500 h-screen pt-16 border-r-2 border-gray-400 p-4 left-0 top-0 ">
+				<ul className="flex flex-col gap-7">
+					<li className="shadow-xl p-2 rounded-2xl bg-slate-100 font-standard text-slate-800 text-2xl font-bold cursor-pointer hover:text-slate-900 hover:scale-110 transition ease-in-out hover:underline hover:underline-offset-8">
+						من نحن{" "}
+					</li>
+					<li className="shadow-xl p-2 rounded-2xl bg-slate-100 font-standard text-slate-800 text-2xl font-bold cursor-pointer hover:text-slate-900 hover:scale-110 transition ease-in-out hover:underline hover:underline-offset-8">
+						القطاعات
+					</li>
+					<li className="shadow-xl p-2 rounded-2xl bg-slate-100 font-standard text-slate-800 text-2xl font-bold cursor-pointer hover:text-slate-900 hover:scale-110 transition ease-in-out hover:underline hover:underline-offset-8">
+						تواصل معنا
+					</li>
+				</ul>
+			</div> */}
 		</div>
 	);
 };
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,18 +1,68 @@
-import React from "react";
+import { delay } from "q";
+import React, { useState } from "react";
+import { BsChevronCompactLeft, BsChevronCompactRight } from "react-icons/bs";
+
+const slides = [
+	{
+		url: "https://gate.ahram.org.eg/Media/News/2021/7/14/19_2021-637618585878648761-864.jpg",
+	},
+	{
+		url: "https://scontent-hbe1-1.xx.fbcdn.net/v/t1.6435-9/131144105_422624392452083_1599017125498183165_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=730e14&_nc_ohc=IpwyZarC9ucAX9f3Wia&_nc_ht=scontent-hbe1-1.xx&oh=00_AfAQhuKZxBoi5N_J1YqeYjgdUs4C_FYEe6veTZ2KOQuOXA&oe=6432992C",
+	},
+	{
+		url: "https://scontent-hbe1-1.xx.fbcdn.net/v/t1.6435-9/71136482_127476365300222_579715040433668096_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=730e14&_nc_ohc=cpkyTtY8X2sAX-KkDeg&_nc_ht=scontent-hbe1-1.xx&oh=00_AfBFDWaEAbzW_ufrleHpnKaJqrKAUnuLUMW9YJ09SAwYIQ&oe=6432A7B1",
+	},
+	{
+		url: "https://scontent.fcai20-2.fna.fbcdn.net/v/t1.6435-9/71871370_127476141966911_7806580312950964224_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=730e14&_nc_ohc=RVXIEGv3xlkAX_QLGOc&_nc_ht=scontent.fcai20-2.fna&oh=00_AfCIQaeIYcDOL6CTP2PD9XRt7dFQazJ0PlzOH7s9f2P3Vg&oe=6432A135",
+	},
+	{
+		url: "https://scontent.fcai20-2.fna.fbcdn.net/v/t1.6435-9/70462679_120086712705854_1670977376063848448_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=730e14&_nc_ohc=yCUSxHDMtkwAX9yBKlw&_nc_ht=scontent.fcai20-2.fna&oh=00_AfCUdsJdupAN7wr1Am7PLeEr-Bwdta_1grkmEx8Xp2vvfw&oe=6432AE6A",
+	},
+];
 
 const Hero = () => {
+	const [currentSlide, setCurrentSlide] = useState(0);
+
+	const PrevSlide = () => {
+		const isFirstslide = currentSlide === 0;
+		const newSlide = isFirstslide ? slides.length - 1 : currentSlide - 1;
+		setCurrentSlide(newSlide);
+	};
+	const NextSlide = () => {
+		const isLastSlide = currentSlide === slides.length - 1;
+		const newSlide = isLastSlide ? 0 : currentSlide + 1;
+		setCurrentSlide(newSlide);
+	};
 	return (
-		<div>
+		<div className="p-4 relative border-b-4 border-slate-600 pb-4 group">
 			{/* //! The Image  */}
-			<div className="w-full">
+			<div className="w-full flex items-center justify-center relative h-10/12 blur-sm rounded-2xl bg-center bg-cover duration-500 h-full">
 				<img
-					src="https://misrelzraea.com/wp-content/uploads/2022/01/%D9%85%D8%B4%D8%B1%D9%88%D8%B9-%D8%A7%D9%84%D8%B5%D9%88%D8%A8-%D8%A7%D9%84%D8%B2%D8%B1%D8%A7%D8%B9%D9%8A%D8%A9-%D9%88%D8%A3%D9%87%D9%85-%D8%A3%D9%87%D8%AF%D8%A7%D9%81%D9%87.jpg"
+					className="h-10/12 blur-sm rounded-2xl bg-center bg-cover duration-500 w-8/12 h-full"
+					// src="https://gate.ahram.org.eg/Media/News/2021/7/14/19_2021-637618585878648761-864.jpg"
+					src={`${slides[currentSlide].url}`}
 					alt="img1"
 				/>
 			</div>
-			<div>الشركة الوطنية للزراعات المحمية</div>
+			{/* //! Cursors for sliding left and right */}
+			<div className="hidder group-hover:block absolute top-[50%] transition ease-in-out  left-5 text-2xl rounded-full p-2 bg-slate-700 text-slate-50 opacity-80 hover:scale-110">
+				<BsChevronCompactLeft
+					onClick={PrevSlide}
+					className="text-2xl  hover:cursor-pointer hover:text-3xl hover:scale-110 transition ease-in-out opacity-100"
+				/>
+			</div>
+			<div className="hidder group-hover:block absolute top-[50%] transition ease-in-out  right-5 text-2xl rounded-full p-2 bg-slate-700 text-slate-50 opacity-80 hover:scale-110">
+				<BsChevronCompactRight
+					onClick={NextSlide}
+					className="text-2xl  hover:cursor-pointer hover:text-3xl hover:scale-110 transition ease-in-out opacity-100"
+				/>
+			</div>
+			{/* //! Text inside the image slider */}
+			<div className="hover:cursor-pointer hover:scale-110 transition ease-in-out flex flex-col gap-y-2 items-center justify-center md:text-2xl  lg:text-4xl top-1/2 left-1/2 absolute font-bold -translate-x-1/2 -translate-y-3/4  shadow-2xl p-2 ">
+				<p>الشركة الوطنية للزراعات المحمية</p>
+				<p>NCPC</p>
+			</div>
 		</div>
 	);
 };
-
 export default Hero;
