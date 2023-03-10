82f638e557aaccc072c0e662e93de1fd432bc98e
fady <gamilfady605@gmail.com>
2023-03-10 12:04:07 +0200

    setup the app

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+	return (
+		<div className="container columns-md grid grid-cols-2">
+			<div>
+				<img
+					src="https://www.egypttoday.com/siteimages/Larg/65402.jpg"
+					alt="img1"
+				/>
+			</div>
+			<div>الشركة الوطنية للزراعات المحمية</div>
+		</div>
+	);
+}
+
+export default App;
